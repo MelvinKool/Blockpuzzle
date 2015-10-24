2d59f621f88c5ab33ae4cc427429180fa8dbc7e8
epicbro97 <kool1403@student.nhl.nl>
2015-10-24 22:49:49 +0200

    making gui ready for placing poles

--- BlockPuzzle/src/blockpuzzle/BlockPuzzleBoard.java
+++ BlockPuzzle/src/blockpuzzle/BlockPuzzleBoard.java
@@ -2,28 +2,29 @@ package blockpuzzle;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Point;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JPanel;
 
 public class BlockPuzzleBoard extends JPanel{
 	private final int PANELSIZE;
-	private final int AMOUNT_BLOCKS;
-	private final int CELLSIZE;
+	public final int AMOUNT_BLOCKS;
+	public final int CELLSIZE;
+	public List<Point> polePlaces = new ArrayList<Point>();
 	
 	public BlockPuzzleBoard(int panelSize,int numBlocks){
 		PANELSIZE = panelSize;
 		AMOUNT_BLOCKS = numBlocks;
 		CELLSIZE = panelSize/numBlocks;
+		this.setBackground(Color.WHITE);
 	}
 	
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
-		this.setBackground(Color.WHITE);
-		g.setColor(Color.BLUE);
-		g.fillRect(0, 0, CELLSIZE, CELLSIZE);
-		g.setColor(Color.CYAN);
-		g.fillRect(CELLSIZE, CELLSIZE, CELLSIZE, CELLSIZE);
 	}
+	
 }
